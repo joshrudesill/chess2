f383fed42c5a3676a93ce02db569de2399be25af
Joshua Rudesill <joshrudesill@gmail.com>
2022-11-08 16:05:29 +0100

    more socket config

--- pages/findmatch.js
+++ pages/findmatch.js
@@ -14,19 +14,51 @@ const FindMatch = () => {
       }
     );
     const session = await result.json();
-    return session;
+    if (session) {
+      console.log(session);
+      const sid = session.sessionID;
+      const uid = session.userID;
+      const un = session.username;
+      socket.auth = { sid, uid, un };
+      socket.connect();
+    } else {
+      const un = "testUser";
+      socket.auth = { un };
+      socket.connect();
+    }
+  };
+  const saveSessionDetails = async (session) => {
+    const result = await fetch(`http://localhost:8080/savesession`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(session),
+    });
+    if (result.status === 200) {
+      socket.emit();
+    }
   };
   useEffect(() => {
-    const sessionID = localStorage.getItem("sessionID");
-    if (sessionID) {
-      const sid = loadSessionDetails(sessionID);
+    const sid = localStorage.getItem("sessionID");
+    if (sid) {
       socket.auth = { sid };
       socket.connect();
     } else {
-      const username = "testUser";
-      socket.auth = { username };
+      const un = "testUser";
+      socket.auth = { un };
       socket.connect();
     }
+    socket.on("connect_error", (err) => {
+      if (err.message === "db required") {
+        loadSessionDetails(err.data.content);
+      }
+    });
+    socket.on("session started", (sid, uid, un) => {
+      localStorage.setItem("sessionID", sid);
+      saveSessionDetails({ sid: sid, uid: uid, un: un });
+    });
   }, []);
   return <div></div>;
 };
