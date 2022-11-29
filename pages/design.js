9a1ee6acab108aa39bf5e3c1148b0463da96da57
Joshua Rudesill <joshrudesill@gmail.com>
2022-11-29 15:41:46 +0100

    style

--- pages/design.js
+++ pages/design.js
@@ -25,8 +25,8 @@ const Design = () => {
     }
   }, [height, width]);
   return (
-    <div className='flex flex-row gap-5'>
-      <div className='hidden md:flex fixed flex-col w-36 bg-neutral-700 h-screen'>
+    <div className='flex flex-row gap-5 overflow-x-hidden'>
+      <div className='hidden md:flex fixed flex-col w-36 bg-neutral-700 h-screen '>
         <div className='w-36 flex-initial flex hover:bg-neutral-500 p-3'>
           {boardSize}
         </div>
@@ -35,9 +35,9 @@ const Design = () => {
           Play
         </div>
       </div>
-      <div className='flex md:w-screen flex-col lg:flex-row gap-3 ml-48'>
+      <div className='flex w-[85vmin] md:w-max mx-auto md:mx-0 flex-col lg:flex-row gap-3 md:ml-48 overflow-x-hidden'>
         <div className='flex flex-col md:flex-row justify-center gap-3 mt-8'>
-          <div className='md:shrink w-[100%] md:w-[88vmin]'>
+          <div className='md:shrink w-[100%] md:w-[88vmin] overflow-x-hidden'>
             <div className='grid grid-cols-8 grid-rows-8'>
               {a.map((j) => {
                 return a.map((e) => (
@@ -57,27 +57,56 @@ const Design = () => {
             </div>
           </div>
         </div>
-        <div className='w-auto border rounded-md border-neutral-600 divide-neutral-600 shadow-lg flex flex-col divide-y p-1 text-white mt-8'>
-          <div className='flex justify-between p-2'>
-            <p className='font-mono md:text-2xl lg:text-3xl mr-4'>10:00</p>
-            <p className='font-mono md:text-2xl text-neutral-400 lg:text-3xl ml-4'>
+        <div className='lg:w-[30vw] w-11/12 border rounded-md border-neutral-600 divide-neutral-600 shadow-lg flex flex-col divide-y  text-white mx-auto md:mx-0 lg:mt-8 lg:mr-2'>
+          <div className='flex justify-between p-3'>
+            <p className='font-mono md:text-2xl lg:text-xl mr-4'>10:00</p>
+            <p className='font-mono md:text-2xl text-neutral-400 lg:text-xl ml-4'>
               10:00
             </p>
           </div>
-          <div className='flex flex-row justify-center gap-x-1 p-2 '>
-            <p className='font-sans lg:text-xl '>Player1</p>
-            <p className='font-mono lg:text-1xl grow text-center'>VS</p>
-            <p className='font-sans lg:text-xl'>Player2</p>
+          <div className='flex flex-row justify-center gap-x-1 p-3 text-xs'>
+            <p className='font-sans xl:text-lg '>Player1</p>
+            <p className='font-mono xl:text-xl grow text-center'>VS</p>
+            <p className='font-sans xl:text-lg'>Player2</p>
           </div>
-          <div className='flex grow flex-row p-2 flex-wrap content-start text-xs'>
-            <div className='flex m-1 bg-neutral-700 p-1 lg:p-2 rounded'>
-              1. asdf
+          <div className='flex grow flex-row flex-wrap content-start text-xs'>
+            <div className='flex bg-neutral-700 w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex bg-neutral-700 w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex bg-neutral-700 w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex bg-neutral-700 w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
+            </div>
+            <div className='flex w-full p-1 justify-between'>
+              <div>1. Kxb2</div>
+              <div>1.3s</div>
             </div>
           </div>
-          <div className='flex flex-row flex-wrap'>
+          <div className='flex flex-row flex-wrap p-1'>
             <button
               type='button'
-              className='inline-flex text-red-700 hover:text-white border border-red-700 hover:bg-red-800 font-medium rounded-lg text-xs px-4 py-2 text-center mx-1 my-2'
+              className='inline-flex text-red-700 hover:text-white border border-red-700 hover:bg-red-800 font-medium rounded-lg text-xs px-2 py-2 text-center mx-1 my-2'
             >
               Resign
               <FlagIcon className='h-4 w-4 text-white ml-2' />
@@ -85,16 +114,7 @@ const Design = () => {
             <Tooltip content='EX' style='light' className='text-xs'>
               <button
                 type='button'
-                className='inline-flex text-yellow-500 hover:text-white border border-yellow-600 hover:bg-yellow-600 font-medium rounded-lg text-xs px-4 py-2 text-center mx-1 my-2'
-              >
-                Draw
-                <HandThumbUpIcon className='h-4 w-4 text-white ml-2' />
-              </button>
-            </Tooltip>
-            <Tooltip content='EX' style='light' className='text-xs'>
-              <button
-                type='button'
-                className='inline-flex text-yellow-500 hover:text-white border border-yellow-600 hover:bg-yellow-600 font-medium rounded-lg text-xs px-4 py-2 text-center mx-1 my-2'
+                className='inline-flex text-yellow-500 hover:text-white border border-yellow-600 hover:bg-yellow-600 font-medium rounded-lg text-xs px-2 py-2 text-center mx-1 my-2'
               >
                 Draw
                 <HandThumbUpIcon className='h-4 w-4 text-white ml-2' />
@@ -102,7 +122,9 @@ const Design = () => {
             </Tooltip>
           </div>
           <div className='flex p-2'>
-            <p className='font-mono lg:text-xl underline '>Variant name</p>
+            <p className='font-mono text-s lg:text-lg underline '>
+              Variant name
+            </p>
           </div>
         </div>
       </div>
