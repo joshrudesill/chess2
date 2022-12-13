736a694b0a12b63b5815aa28bd1608da85c99411
Joshua Rudesill <joshrudesill@gmail.com>
2022-12-13 15:32:25 +0100

    more errors

--- socket.js
+++ socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const URL = "https://chess2server.fly.dev";
-const socket = io(URL, { autoConnect: false });
+const socket = io(URL, { autoConnect: false, withCredentials: true });
 socket.onAny((event, ...args) => {
   console.log(event, args);
 });
