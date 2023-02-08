5d65de95165ff6a4bf889f265a55308a43c38740
Joshua Rudesill <joshrudesill@gmail.com>
2023-02-08 12:52:56 +0100

    front page mostly done

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,24 +1,32 @@
+import { TrophyIcon } from "@heroicons/react/20/solid";
 import {
-  FlagIcon,
-  HandThumbUpIcon,
-  TrophyIcon,
-  InformationCircleIcon,
-} from "@heroicons/react/20/solid";
+  MarkGithubIcon,
+  SmileyIcon,
+  CodeReviewIcon,
+  LogIcon,
+} from "@primer/octicons-react";
 const Sidebar = () => {
   return (
-    <div className='hidden md:flex flex-col w-36 bg-neutral-800 h-screen left-0'>
-      <div className='w-36 flex-initial flex hover:bg-neutral-500 p-3'>asd</div>
-      <div className='w-36 inline-flex hover:bg-neutral-500 p-3 font-semibold text-white hover:skew-y-3 cursor-pointer text-md'>
-        <TrophyIcon className='h-6 w-6 mt-0.5 mr-2 text-green-500 ' />
-        Play
+    <div className='hidden md:flex flex-col w-36 bg-neutral-800 h-screen left-0 px-2 pt-3 justify-start'>
+      <div className='flex flex-row  text-white cursor-pointer text-lg hover:bg-lime-500 rounded-lg p-3  '>
+        <TrophyIcon className='h-6 w-6 mt-0.5 mr-2 ' />
+        Home
       </div>
-      <div className='w-36 inline-flex hover:bg-neutral-500 p-3 font-semibold text-white hover:skew-y-3 cursor-pointer text-md'>
-        <InformationCircleIcon className='h-6 w-6 mt-0.5 mr-2 text-blue-400 ' />
-        More Info
+      <div className='flex flex-row  text-white cursor-pointer text-lg hover:bg-lime-500 rounded-lg p-3'>
+        <MarkGithubIcon size={24} className='mr-3' />
+        Source
       </div>
-      <div className='w-36 inline-flex hover:bg-neutral-500 p-3 font-semibold text-white hover:skew-y-3 cursor-pointer text-md mt-auto'>
-        <InformationCircleIcon className='h-6 w-6 mt-0.5 mr-2 text-blue-400 ' />
-        More Info
+      <div className='flex flex-row  text-white cursor-pointer text-lg hover:bg-lime-500 rounded-lg p-3'>
+        <LogIcon size={24} className='mr-3' />
+        Resume
+      </div>
+      <div className='flex flex-row  text-white cursor-pointer text-lg hover:bg-lime-500 rounded-lg p-3'>
+        <CodeReviewIcon size={24} className='mr-3' />
+        Blog
+      </div>
+      <div className='flex flex-row  text-white cursor-pointer text-lg hover:bg-lime-500 rounded-lg p-3'>
+        <SmileyIcon size={24} className='mr-3' />
+        Contact
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-import { UserCircleIcon } from "@heroicons/react/20/solid";
+import { MagnifyingGlassCircleIcon } from "@heroicons/react/20/solid";
+import {
+  MarkGithubIcon,
+  SmileyIcon,
+  CodeReviewIcon,
+  LogIcon,
+} from "@primer/octicons-react";
 import dayjs from "dayjs";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -10,73 +16,109 @@ const logo = require("../assets/Frame2.svg");
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row w-screen h-screen">
+      <div className='flex flex-row w-screen h-screen'>
         <Sidebar />
-        <div className="flex justify-center h-full grow">
-          <div className="flex rounded-md flex-col gap-5 my-auto h-min w-min p-3">
-            <div className="flex flex-row items-center gap-3 border-b border-neutral-600 pb-1">
-              <div className="flex flex-col p-1">
-                <div className="flex flex-row gap-1">
-                  <span className="text-2xl font-semibold tracking-wider rounded-md text-white align-text-bottom">
+        <div className='flex justify-center h-full grow'>
+          <div className='flex rounded-md flex-col gap-5 my-auto h-min w-min p-3'>
+            <div className='flex flex-row items-center gap-3 border-b border-neutral-600 pb-1'>
+              <div className='flex flex-col p-1'>
+                <div className='flex flex-row gap-1'>
+                  <span className='text-2xl font-semibold tracking-wider rounded-md text-white align-text-bottom select-none'>
                     Chess
                   </span>
-                  <div className="flex flex-col justify-center px-1 rounded-lg text-xs bg-orange-500 text-white">
+                  <div className='flex flex-col justify-center px-1 rounded-lg text-xs bg-orange-500 text-white select-none'>
                     <p>v1.0</p>
                   </div>
                 </div>
-                <div className="flex flex-row text-neutral-200 text-xs">
+                <div className='flex flex-row text-neutral-200 text-xs select-none'>
                   classic edition
                 </div>
               </div>
               <div>
-                <button className="rounded-md bg-lime-600 py-2 px-4 text-lg font-bold text-white border-b-4 border-lime-700 tracking-wider hover:bg-lime-500">
+                <button
+                  className='rounded-md bg-lime-600 py-2 px-4 text-lg font-bold text-white border-b-4 border-lime-700 tracking-wider hover:bg-lime-500'
+                  select-none
+                >
                   Play
                 </button>
               </div>
+              <div className='grow flex justify-end'>
+                <div className='text-white rounded-md border border-lime-700 p-1 flex flex-row text-lg pl-2 select-none'>
+                  in queue
+                  <MagnifyingGlassCircleIcon className='h-6 w-6 text-lime-600 my-auto ml-1 animate-pulse' />
+                </div>
+              </div>
             </div>
-            <div className="flex flex-row gap-3 rounded-3xl border-neutral-800">
-              <div className="flex flex-col justify-center s rounded-3xl basis-1/2 flex-wrap py-5 px-5 shadow-lg bg-neutral-500 border-b-8 border-neutral-800">
-                <div className="flex flex-row gap-1 mx-auto ">
-                  <span className="text-3xl font-semibold tracking-wider rounded-md text-white align-text-bottom">
+            <div className='flex flex-row gap-3 rounded-3xl border-neutral-800'>
+              <div className='flex flex-col justify-center s rounded-3xl basis-1/2 flex-wrap py-5 px-5 shadow-lg bg-neutral-500 border-b-8 border-neutral-800'>
+                <div className='flex flex-row gap-1 mx-auto '>
+                  <span className='text-3xl font-semibold tracking-wider rounded-md text-white align-text-bottom select-none'>
                     Chess
                   </span>
-                  <div className="flex flex-col justify-center px-2 rounded-xl text-md bg-teal-500 text-white border-b-4 border-teal-700">
+                  <div className='flex flex-col justify-center px-2 rounded-xl text-md bg-teal-500 text-white border-b-4 border-teal-700 select-none'>
                     <p>v2.0</p>
                   </div>
                 </div>
-                <h2 className="flex justify-center w-full font-bold text-sm ">
+                <h2 className='flex justify-center w-full font-bold text-sm  select-none'>
                   coming soon
                 </h2>
               </div>
-              <div className="relative rounded-3xl basis-1/2 w-72 h-48 ">
+              <div className='relative rounded-3xl basis-1/2 w-72 h-48 '>
                 <Image
                   src={logo}
-                  alt="chess2logo"
-                  layout="fill"
-                  objectFit="contain"
+                  alt='chess2logo'
+                  layout='fill'
+                  objectFit='contain'
                 ></Image>
               </div>
             </div>
-            <div>
-              Chess2 is a project made by me, Josh Rudesill for my web
-              development portfolio. It is built using React / Next.js,
-              Websockets (socket.io), Express.js, MongoDB and Tailwind.
-              <br />
-              This project is still being actively developed! Report bugs on
-              GitHub
+            <div className='text-white tracking-wide rounded-md border-2 border-neutral-500 flex justify-between'>
+              <div className='p-4 grow max-w-[95%]'>
+                Chess2 is a project made by me, Josh Rudesill for my web
+                development portfolio. It's built using React / Next, Websockets
+                (socket.io), Express, MongoDB and Tailwind.
+              </div>
+              <div className='border-l-2 border-neutral-500 bg-neutral-500 w-3'></div>
             </div>
-            <div className="flex flex-row justify-between p-2">
-              <button className="rounded-sm bg-green-500 py-2 px-4">
-                link
+            <div className='border-2 rounded-md border-red-500 text-white font-semibold tracking-wide flex'>
+              <div className='grow p-2'>
+                This project is still being actively developed! <br />
+                Report bugs on GitHub
+              </div>
+              <div className='border-l-2 border-red-500 bg-red-500 w-3' />
+            </div>
+            <div className='flex flex-row justify-between p-2'>
+              <button className='rounded-md bg-teal-600 py-2 px-2 text-white font-semibold tracking-wide flex flex-row text-sm hover:bg-teal-700'>
+                <MarkGithubIcon
+                  verticalAlign='middle'
+                  size={16}
+                  className='my-auto mr-2'
+                />
+                <p className='my-auto'>Source</p>
               </button>
-              <button className="rounded-sm bg-green-500 py-2 px-4">
-                link
+              <button className='rounded-md bg-teal-600 py-2 px-2 text-white font-semibold tracking-wide flex flex-row text-sm hover:bg-teal-700'>
+                <SmileyIcon
+                  verticalAlign='middle'
+                  size={16}
+                  className='my-auto mr-2'
+                />
+                <p className='my-auto'>Contact Me</p>
               </button>
-              <button className="rounded-sm bg-green-500 py-2 px-4">
-                link
+              <button className='rounded-md bg-teal-600 py-2 px-2 text-white font-semibold tracking-wide flex flex-row text-sm hover:bg-teal-700'>
+                <CodeReviewIcon
+                  verticalAlign='middle'
+                  size={16}
+                  className='my-auto mr-2'
+                />
+                <p className='my-auto'>Blog</p>
               </button>
-              <button className="rounded-sm bg-green-500 py-2 px-4">
-                link
+              <button className='rounded-md bg-teal-600 py-2 px-2 text-white font-semibold tracking-wide flex flex-row text-sm hover:bg-teal-700'>
+                <LogIcon
+                  verticalAlign='middle'
+                  size={16}
+                  className='my-auto mr-2'
+                />
+                <p className='my-auto'>Resume</p>
               </button>
             </div>
           </div>
