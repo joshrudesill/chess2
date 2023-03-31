6212e444ac75de6a385683d614f1243c45017642
Joshua Rudesill <joshrudesill@gmail.com>
2023-03-31 10:53:43 -0500

    test component

--- pages/testing.js
+++ pages/testing.js
@@ -1,20 +1,49 @@
-import { useEffect, useState, useLayoutEffect } from "react";
-import useScreenSize from "../util/usescreensize";
+import { useEffect, useState } from "react";
 
-const Testing = () => {
-  const { width, height } = useScreenSize();
-  useLayoutEffect(() => {}, []);
+const Voting = () => {
+  //use state is a react hook for capturing state. it returns the state and a setter for the state as you can see in the destructured array
+  const [upvoted, setUpvoted] = useState(false);
+  const [downvoted, setDownvoted] = useState(false);
+
+  const upvote = () => {
+    setDownvoted(false);
+    setUpvoted(!upvoted); // we dont set this to true because if its already true we want it to get set to false therefore we just set it to the opposite of itself
+  };
+  const downvote = () => {
+    setUpvoted(false);
+    setDownvoted(!downvoted);
+  };
+
+  const callAPI = () => {
+    fetch(
+      `https://yourapi.com/upvote+upvote=${
+        upvoted ? "true" : "false"
+      }&downvote=${downvoted ? "true" : "false"}`
+    );
+  };
+
+  // a useeffect will watch the two variables (upvoted and downvoted) for state changes. once they change from a user click if either is true then we send a api call
+  useEffect(() => {
+    if (upvoted || downvoted) {
+      callAPI();
+    }
+  }, [upvoted, downvoted]);
+
+  // Then render with ternary operators and put on click attributes on the parent divs
   return (
-    <div className='flex flex-row gap-3 border h-screen'>
-      <div className='fixed h-screen w-36 bg-red-400'></div>
-      <div className='w-[min(90vh,90vw)] bg-green-400 ml-48'>
-        <div className='w-full bg-yellow-400'></div>
+    <>
+      <div onClick={() => upvote()}>
+        {upvoted ? <UpvoteColoredComponent /> : <UpvoteStandardComponent />}
       </div>
-      <div className='w-auto bg-blue-400 text-white'>
-        hasdfasdfasdhasdfasdfasdhasdfasdfasdhasdfasdfasd
+      <div onClick={() => downvote()}>
+        {downvoted ? (
+          <DownvoteColoredComponent />
+        ) : (
+          <DownvoteStandardComponent />
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
-export default Testing;
+export default Voting;
